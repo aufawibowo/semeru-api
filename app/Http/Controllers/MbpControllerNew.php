d2027fa1acaa778561db44eca4f38b9d3dfcba5b
Aufa Wibowo <muhammad.aufa16@mhs.if.its.ac.id>
2019-11-27 18:43:53 +0700

    draft

--- app/Http/Controllers/MbpControllerNew.php
+++ app/Http/Controllers/MbpControllerNew.php
@@ -23,12 +23,24 @@ class MbpControllerNew extends Controller
 
     if ($mbp_data) {
       if ($mbp_data->submission=='DELAY') {
-        $user_mbp_data = DB::table('mbp as m')
+        /*
+        select *
+        from mbp as m
+        join rtpo as rh on m.rtpo_id_home = rh.rtpo_id
+        join rtpo as rn  on  m.rtpo_id = rn.rtpo_id
+        join user_mbp as um  on m.mbp_id = um.mbp_id
+        join users as u  on um.username = u.username
+        join supplying_power as sp on m.mbp_id = sp.mbp_id
+        join site as s on sp.site_id = s.site_id
+        join message as msg on m.message_id = msg.id
+        where sp.finish = NULL
+        */
+        $result = DB::table('mbp as m')
                         ->join('rtpo as rh', 'm.rtpo_id_home', '=', 'rh.rtpo_id')
                         ->join('rtpo as rn', 'm.rtpo_id', '=', 'rn.rtpo_id')
                         ->join('user_mbp as um', 'm.mbp_id', '=', 'um.mbp_id')
                         ->join('users as u', 'um.username', '=', 'u.username')
-                        ->join('supplying_power as sp', '=', 'm.mbp_id', 'sp.mbp_id')
+                        ->join('supplying_power as sp', 'm.mbp_id', '=', 'sp.mbp_id')
                         ->join('site as s', 'sp.site_id', '=', 's.site_id')
                         ->join('message as msg', 'm.message_id', '=', 'msg.id')
                         ->select('*', 'm.status as mbp_status', 's.latitude as site_latitude', 's.longitude as site_longitude', 'm.rtpo_id as mbp_rtpo_id', 'm.rtpo_id_home as mbp_rtpo_id_home', 'rh.rtpo_name as rtpo_name_home', 'rn.rtpo_name as rtpo_name_now','sp.user_rtpo_cn as ticket_by')
@@ -36,51 +48,50 @@ class MbpControllerNew extends Controller
                         ->where('sp.finish','=',null)
                         ->first();
 
-        $result = json_decode($btss, "OK");
+        $result = json_decode($user_mbp_data, "OK");
         if ($result==NULL) {
           $res['success'] = "OK";
           $res['message'] = 'Success';
-          $res['total_page'] = $btss_count;
-          $res['data'] = $btss;
+          $res['data'] = $user_mbp_data;
           return response($res);
         }
  
-        // $data['status'] = @$user_mbp_data->mbp_status;
-        $data['ticket_by'] = $user_mbp_data->ticket_by;
-        $data['telegram_username'] = "";
-        $data['status'] = 'DELAY';
-        $data['mbp_name'] = @$user_mbp_data->mbp_name;
-        $data['name'] = @$user_mbp_data->name;
-        $data['phone'] = @$user_mbp_data->phone;
-        $data['mbp_latitude'] = @$user_mbp_data->latitude;
-        $data['mbp_longitude'] = @$user_mbp_data->longitude;
-        $data['site_name'] = @$user_mbp_data->site_name;
-        $data['code_name'] = @$user_mbp_data->site_id;
-        $data['class_name'] = @$user_mbp_data->site_class;
-        $data['latitude'] = @$user_mbp_data->site_latitude;
-        $data['longitude'] = @$user_mbp_data->site_longitude;
-        $data['borrowed'] = @$borrowed;
-        $data['date_waiting'] = @strtotime(@$user_mbp_data->date_waiting);
-        $data['date_onprogress'] = @strtotime(@$user_mbp_data->date_onprogress);
-        $data['date_checkin'] = @strtotime(@$user_mbp_data->date_checkin);
+        foreach ($result as $param => $row) {
+          // $data['status'] = mbp_status;
+          $data[$param]['ticket_by']           = $row['ticket_by'];
+          $data[$param]['telegram_username']   = "";
+          $data[$param]['status']              = 'DELAY';
+          $data[$param]['mbp_name']            = $row['mbp_name'];
+          $data[$param]['name']                = $row['name'];
+          $data[$param]['phone']               = $row['phone'];
+          $data[$param]['mbp_latitude']        = $row['latitude'];
+          $data[$param]['mbp_longitude']       = $row['longitude'];
+          $data[$param]['site_name']           = $row['site_name'];
+          $data[$param]['code_name']           = $row['site_id'];
+          $data[$param]['class_name']          = $row['site_class'];
+          $data[$param]['latitude']            = $row['site_latitude'];
+          $data[$param]['longitude']           = $row['site_longitude'];
+          $data[$param]['borrowed']            = $borrowed;
+          $data[$param]['date_waiting']        = strtotime($row['date_waiting']);
+          $data[$param]['date_onprogress']     = strtotime($row['date_onprogress']);
+          $data[$param]['date_checkin']        = strtotime($row['date_checkin']);
+          $data[$param]['fmc_id']              = $row['fmc_id'];
+          $data[$param]['fmc_name']            = $row['fmc'];
+          $data[$param]['rtpo_id_home']        = $row['mbp_rtpo_id_home'];
+          $data[$param]['rtpo_id_now']         = $row['mbp_rtpo_id'];
+          $data[$param]['rtpo_name_home']      = $row['rtpo_name_home'];
+          $data[$param]['rtpo_name_now']       = $row['rtpo_name_now'];
+          $data[$param]['subject']             = $row['subject'];
+          $data[$param]['text_message']        = $row['text_message'];
+          $data[$param]['time']                = setDatedMYHis($row['active_at']);
+        }
 
-        $data['fmc_id'] = @$user_mbp_data->fmc_id;
-        $data['fmc_name'] = @$user_mbp_data->fmc;
-
-        $data['rtpo_id_home'] = @$user_mbp_data->mbp_rtpo_id_home;
-        $data['rtpo_id_now'] = @$user_mbp_data->mbp_rtpo_id;
-        $data['rtpo_name_home'] = @$user_mbp_data->rtpo_name_home;
-        $data['rtpo_name_now'] = @$user_mbp_data->rtpo_name_now;
-        $data['subject'] = @$user_mbp_data->subject;
-        $data['text_message'] = @$user_mbp_data->text_message;
-        $data['time'] = @$this->setDatedMYHis($user_mbp_data->active_at);
-
-
-
-        $res['success'] = "OK";
-        $res['message'] = 'Success';
-        $res['data'] = $data;
-        return response($res);
+        if ($result) {
+          $res['success'] = "OK";
+          $res['message'] = 'Success';
+          $res['data'] = $user_mbp_data;
+          return response($res);
+        }
       }
       switch ($mbp_data->status) {
         case "AVAILABLE":
@@ -94,22 +105,22 @@ class MbpControllerNew extends Controller
           ->first();
 
           $data['get_in'] = 'AVAILABLE';
-          $data['status'] = $user_mbp_data->mbp_status;
-          $data['mbp_name'] = $user_mbp_data->mbp_name;
-          $data['name'] = $user_mbp_data->name;
-          $data['phone'] = $user_mbp_data->phone;
-          $data['latitude'] = $user_mbp_data->latitude;
-          $data['longitude'] = $user_mbp_data->longitude;
+          $data['status'] = mbp_status;
+          $data['mbp_name'] = mbp_name;
+          $data['name'] = name;
+          $data['phone'] = phone;
+          $data['latitude'] = latitude;
+          $data['longitude'] = longitude;
           $data['borrowed'] = $borrowed;
           $data['class_name'] = '-';
 
-          $data['fmc_id'] = $user_mbp_data->fmc_id;
-          $data['fmc_name'] = $user_mbp_data->fmc;
+          $data['fmc_id'] = fmc_id;
+          $data['fmc_name'] = fmc;
           
-          $data['rtpo_id_home'] = $user_mbp_data->mbp_rtpo_id_home;
-          $data['rtpo_id_now'] = $user_mbp_data->mbp_rtpo_id;
-          $data['rtpo_name_home'] = $user_mbp_data->rtpo_name_home;
-          $data['rtpo_name_now'] = $user_mbp_data->rtpo_name_now;
+          $data['rtpo_id_home'] = mbp_rtpo_id_home;
+          $data['rtpo_id_now'] = mbp_rtpo_id;
+          $data['rtpo_name_home'] = rtpo_name_home;
+          $data['rtpo_name_now'] = rtpo_name_now;
 
           $res['success'] = "OK";
           $res['message'] = 'Success';
@@ -135,29 +146,29 @@ class MbpControllerNew extends Controller
           $data['borrowed'] = $borrowed;
           $data['class_name'] = '-';
 
-          $data['fmc_id'] = @$user_mbp_data->fmc_id;
-          $data['fmc_name'] = @$user_mbp_data->fmc;
+          $data['fmc_id'] = fmc_id;
+          $data['fmc_name'] = fmc;
 
-          $data['rtpo_id_home'] = @$user_mbp_data->mbp_rtpo_id_home;
-          $data['rtpo_id_now'] = @$user_mbp_data->mbp_rtpo_id;
-          $data['rtpo_name_home'] = @$user_mbp_data->rtpo_name_home;
-          $data['rtpo_name_now'] = @$user_mbp_data->rtpo_name_now;
+          $data['rtpo_id_home'] = mbp_rtpo_id_home;
+          $data['rtpo_id_now'] = mbp_rtpo_id;
+          $data['rtpo_name_home'] = rtpo_name_home;
+          $data['rtpo_name_now'] = rtpo_name_now;
 
-          $data['mbp_name'] = @$user_mbp_data->mbp_name;
-          $data['name'] = @$user_mbp_data->name;
-          $data['phone'] = @$user_mbp_data->phone;
-          $data['latitude'] = @$user_mbp_data->latitude;
-          $data['longitude'] = @$user_mbp_data->longitude;
-          $data['subject'] = @$user_mbp_data->subject;
-          $data['text_message'] = @$user_mbp_data->text_message;
-          $data['time'] = @$this->setDatedMYHis($user_mbp_data->active_at);
+          $data['mbp_name'] = mbp_name;
+          $data['name'] = name;
+          $data['phone'] = phone;
+          $data['latitude'] = latitude;
+          $data['longitude'] = longitude;
+          $data['subject'] = subject;
+          $data['text_message'] = text_message;
+          $data['time'] = $this->setDatedMYHis(active_at);
 
           // $data['time a'] = date('i');
 
-          // $dateb = strtotime(@$user_mbp_data->lu);
+          // $dateb = strtotime(lu);
           // $data['time b'] = date('i', $dateb);
           // $data['time c'] = date('i', $dateb) + 1;
-          // $data['time lu'] = @$user_mbp_data->lu;
+          // $data['time lu'] = lu;
 
           //    if (date('i') == date('i', $mbp_trouble->respon_date)) {//----------------------
           //   $res['success'] = true;
@@ -187,31 +198,31 @@ class MbpControllerNew extends Controller
           ->first();
 
           $data['get_in'] = "DEFAULT";
-          $data['ticket_by'] = $user_mbp_data->ticket_by;
+          $data['ticket_by'] = ticket_by;
           $data['telegram_username'] = "";
-          $data['status'] = $user_mbp_data->mbp_status;
-          $data['mbp_name'] = $user_mbp_data->mbp_name;
-          $data['name'] = $user_mbp_data->name;
-          $data['phone'] = $user_mbp_data->phone;
-          $data['mbp_latitude'] = $user_mbp_data->latitude;
-          $data['mbp_longitude'] = $user_mbp_data->longitude;
-          $data['site_name'] = $user_mbp_data->site_name;
-          $data['code_name'] = $user_mbp_data->site_id;
-          $data['class_name'] = $user_mbp_data->site_class;
-          $data['latitude'] = $user_mbp_data->site_latitude;
-          $data['longitude'] = $user_mbp_data->site_longitude;
+          $data['status'] = mbp_status;
+          $data['mbp_name'] = mbp_name;
+          $data['name'] = name;
+          $data['phone'] = phone;
+          $data['mbp_latitude'] = latitude;
+          $data['mbp_longitude'] = longitude;
+          $data['site_name'] = site_name;
+          $data['code_name'] = site_id;
+          $data['class_name'] = site_class;
+          $data['latitude'] = site_latitude;
+          $data['longitude'] = site_longitude;
           $data['borrowed'] = $borrowed;
-          $data['date_waiting'] = @strtotime(@$user_mbp_data->date_waiting);
-          $data['date_onprogress'] = @strtotime(@$user_mbp_data->date_onprogress);
-          $data['date_checkin'] = @strtotime(@$user_mbp_data->date_checkin);
+          $data['date_waiting'] = strtotime(date_waiting);
+          $data['date_onprogress'] = strtotime(date_onprogress);
+          $data['date_checkin'] = strtotime(date_checkin);
 
-          $data['fmc_id'] = $user_mbp_data->fmc_id;
-          $data['fmc_name'] = $user_mbp_data->fmc;
+          $data['fmc_id'] = fmc_id;
+          $data['fmc_name'] = fmc;
 
-          $data['rtpo_id_home'] = $user_mbp_data->mbp_rtpo_id_home;
-          $data['rtpo_id_now'] = $user_mbp_data->mbp_rtpo_id;
-          $data['rtpo_name_home'] = $user_mbp_data->rtpo_name_home;
-          $data['rtpo_name_now'] = $user_mbp_data->rtpo_name_now;
+          $data['rtpo_id_home'] = mbp_rtpo_id_home;
+          $data['rtpo_id_now'] = mbp_rtpo_id;
+          $data['rtpo_name_home'] = rtpo_name_home;
+          $data['rtpo_name_now'] = rtpo_name_now;
 
           $res['success'] = "OK";
           $res['message'] = 'Success';
@@ -219,6 +230,18 @@ class MbpControllerNew extends Controller
           return response($res);
         break;
       }
+    }
+  }
+
+  public function setDatedMYHis($date){
+    if ($date==null) {
+      return "";
+    }else if ($date=='0000-00-00 00:00:00') {
+      return "";
+    }else{
+      // return date("d-M-Y H:i:s", strtotime($date.''));
+      return date("d M Y, H:i", strtotime($date.''));
+        // return strtotime($date.'');
     }
   }
 
