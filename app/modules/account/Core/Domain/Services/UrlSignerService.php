8ca7c642c48e736ce7c998107d02cbb4f408685c
Aufa Wibowo <aufa.wibowo@qasir.id>
2021-01-28 05:49:16 +0700

    Add psr-4 autoload

--- app/modules/account/Core/Domain/Services/UrlSignerService.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace A7Pro\Account\Core\Domain\Services;
-
-interface UrlSignerService
-{
-    public function sign(string $url, \DateTime $expiration): string;
-    public function validate(string $url): bool;
-}
