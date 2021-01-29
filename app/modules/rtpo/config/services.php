291e3c8500109a7e1e181c7b26d156b77ca49093
Aufa Wibowo <aufa.wibowo@qasir.id>
2021-01-29 14:01:31 +0700

    add SqlMobilePartnerRepository to services.php

--- app/modules/rtpo/config/services.php
+++ app/modules/rtpo/config/services.php
@@ -1,37 +1,7 @@
 <?php
 
-use Semeru\Rtpo\Infrastructure\Services\JwtTokenService;
-use Semeru\Rtpo\Infrastructure\Services\HashBasedOtpService;
-use Semeru\Rtpo\Infrastructure\Services\GoSmsService;
-use Semeru\Rtpo\Infrastructure\Services\Md5UrlSignerService;
-use Semeru\Rtpo\Infrastructure\Services\ApiApituService;
-use Semeru\Rtpo\Infrastructure\Persistence\SqlDpcRepository;
-use Semeru\Rtpo\Infrastructure\Persistence\SqlUserRepository;
+use Semeru\Rtpo\Infrastructure\Persistence\SqlMobilePartnerRepository;
 
-$container->setShared('tokenService', function() use ($container) {
-    return new JwtTokenService($container->get('config'));
-});
-
-$container->setShared('otpService', function () use($container) {
-    return new HashBasedOtpService($container->get('config'));
-});
-
-$container->setShared('smsService', function () use($container) {
-    return new GoSmsService($container->get('config'));
-});
-
-$container->setShared('urlSignerService', function () use($container) {
-    return new Md5UrlSignerService($container->get('config'));
-});
-
-$container->setShared('apituService', function() use ($container) {
-    return new ApiApituService($container->get('config'));
-});
-
-$container->setShared('dpcRepository', function() use ($container) {
-    return new SqlDpcRepository($container->get('db'));
-});
-
-$container->setShared('userRepository', function() use ($container) {
-    return new SqlUserRepository($container->get('db'));
+$container->setShared('mobilePartnerRepository', function() use ($container) {
+    return new SqlMobilePartnerRepository($container->get('db'));
 });
