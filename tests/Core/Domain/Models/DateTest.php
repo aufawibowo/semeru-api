57dc29872cd1a457bb284487058c84788956933e
Aufa Wibowo <aufa.wibowo@qasir.id>
2021-01-29 14:01:06 +0700

    add DateTest

---
+++ tests/Core/Domain/Models/DateTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Core\Domain\Models;
+
+use Semeru\Rtpo\Core\Domain\Models\Date;
+use PHPUnit\Framework\TestCase;
+
+class DateTest extends TestCase
+{
+
+    public function testDateEquals()
+    {
+
+    }
+
+    public function test__construct()
+    {
+
+    }
+
+    public function testToDateTimeString()
+    {
+
+    }
+
+    public function testGetMonth()
+    {
+
+    }
+
+    public function testToIsoDateTimeString()
+    {
+
+    }
+
+    public function testToIsoDateString()
+    {
+
+    }
+
+    public function testgetMonthThreeChar()
+    {
+
+    }
+}
