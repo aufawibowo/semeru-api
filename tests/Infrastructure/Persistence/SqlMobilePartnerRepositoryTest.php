fec85ddb33702abd8059566f5cf82e99ebb804ae
Aufa Wibowo <aufa.wibowo@qasir.id>
2021-01-29 14:01:41 +0700

    add SqlMobilePartnerRepositoryTest.php

---
+++ tests/Infrastructure/Persistence/SqlMobilePartnerRepositoryTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Infrastructure\Persistence;
+
+use Semeru\Rtpo\Infrastructure\Persistence\SqlMobilePartnerRepository;
+use PHPUnit\Framework\TestCase;
+
+class SqlMobilePartnerRepositoryTest extends TestCase
+{
+
+    public function testGetAll()
+    {
+
+    }
+}
