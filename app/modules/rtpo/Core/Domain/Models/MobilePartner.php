5f12bf01f31ad18427f79bbd671ee2b52fdebae3
Aufa Wibowo <aufa.wibowo@qasir.id>
2021-01-29 07:13:06 +0700

    add models

---
+++ app/modules/rtpo/Core/Domain/Models/MobilePartner.php
@@ -0,0 +1,100 @@
+<?php
+
+
+namespace Rtpo\Core\Domain\Models;
+
+
+use Semeru\Rtpo\Core\Domain\Models\Coordinate;
+use Semeru\Rtpo\Core\Domain\Models\Date;
+use Semeru\Rtpo\Core\Domain\Models\MobilePartnerId;
+use Semeru\Rtpo\Core\Domain\Models\ZipCode;
+
+class MobilePartner
+{
+    private MobilePartnerId $id;
+    private string $name;
+    private ZipCode $zipCode;
+    private Coordinate $coordinate;
+    private Date $dateCreated;
+    private Date $dateVerified;
+    private Date $dateModified;
+
+    /**
+     * MobilePartner constructor.
+     * @param MobilePartnerId $id
+     * @param string $name
+     * @param ZipCode $zipCode
+     * @param Coordinate $coordinate
+     * @param Date $dateCreated
+     * @param Date $dateVerified
+     * @param Date $dateModified
+     */
+    public function __construct(MobilePartnerId $id, string $name, ZipCode $zipCode, Coordinate $coordinate, Date $dateCreated, Date $dateVerified, Date $dateModified)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->zipCode = $zipCode;
+        $this->coordinate = $coordinate;
+        $this->dateCreated = $dateCreated;
+        $this->dateVerified = $dateVerified;
+        $this->dateModified = $dateModified;
+    }
+
+    /**
+     * @return MobilePartnerId
+     */
+    public function getId(): MobilePartnerId
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return ZipCode
+     */
+    public function getZipCode(): ZipCode
+    {
+        return $this->zipCode;
+    }
+
+    /**
+     * @return Coordinate
+     */
+    public function getCoordinate(): Coordinate
+    {
+        return $this->coordinate;
+    }
+
+    /**
+     * @return Date
+     */
+    public function getDateCreated(): Date
+    {
+        return $this->dateCreated;
+    }
+
+    /**
+     * @return Date
+     */
+    public function getDateVerified(): Date
+    {
+        return $this->dateVerified;
+    }
+
+    /**
+     * @return Date
+     */
+    public function getDateModified(): Date
+    {
+        return $this->dateModified;
+    }
+
+
+}
