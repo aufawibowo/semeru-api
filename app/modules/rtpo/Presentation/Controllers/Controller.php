b1a25548ed37d801848c2ab5e8e3fe81b60b0e95
Aufa Wibowo <aufa.wibowo@qasir.id>
2021-01-29 14:00:31 +0700

    add controller

---
+++ app/modules/rtpo/Presentation/Controllers/Controller.php
@@ -0,0 +1,48 @@
+<?php
+
+
+namespace Semeru\Rtpo\Presentation\Controllers;
+
+
+class Controller extends BaseController
+{
+    public function requestMBPToSiteDownAction()
+    {
+
+    }
+
+    public function approveRescheduleSIKAction()
+    {
+
+    }
+
+    public function cancelRequestMBPAction()
+    {
+
+    }
+
+    public function getDetailRescheduleSIKAction()
+    {
+
+    }
+
+    public function getListRescheduleSIKAction()
+    {
+
+    }
+
+    public function getTiketMBPTidakDikerjakanAction()
+    {
+
+    }
+
+    public function updateRescheduleSIKAction()
+    {
+
+    }
+
+    public function updateRescheduleSIKAction()
+    {
+
+    }
+}
