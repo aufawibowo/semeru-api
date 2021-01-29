655da2a80afa1aa0a82d17dc342fcc785fa6e54a
Aufa Wibowo <aufa.wibowo@qasir.id>
2021-01-29 07:13:25 +0700

    add phpunit MobilePartnerTest

---
+++ tests/Models/MobilePartnerTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Core\Domain\Models;
+
+use Rtpo\Core\Domain\Models\MobilePartner;
+use PHPUnit\Framework\TestCase;
+
+class MobilePartnerTest extends TestCase
+{
+
+    public function testGetName()
+    {
+
+    }
+
+    public function testGetDateCreated()
+    {
+
+    }
+
+    public function test__construct()
+    {
+
+    }
+
+    public function testGetDateVerified()
+    {
+
+    }
+
+    public function testGetId()
+    {
+
+    }
+
+    public function testGetCoordinate()
+    {
+
+    }
+
+    public function testGetZipCode()
+    {
+
+    }
+
+    public function testGetDateModified()
+    {
+
+    }
+}
