5dab8f11ba77c09ce226226391d3889685b48077
Aufa Wibowo <aufa.wibowo@qasir.id>
2021-01-29 14:00:00 +0700

    add new endpoints

--- app/modules/rtpo/Presentation/routes/api.php
+++ app/modules/rtpo/Presentation/routes/api.php
@@ -2,10 +2,59 @@
 
 return [
     [
-        'pattern' => '/vendor/me',
+        'pattern' => '/rtpo/request_mbp_to_site_down',
         'method' => 'GET',
-        'namespace' => 'Semeru\vendor\Presentation\Controllers',
-        'controller' => 'login',
-        'action' => 'getAuthenticatedUser'
+        'namespace' => 'Semeru\Rtpo\Presentation\Controllers',
+        'controller' => 'controller',
+        'action' => 'requestMBPToSiteDown'
+    ],
+    [
+        'pattern' => '/rtpo/request_mbp_to_site_down',
+        'method' => 'GET',
+        'namespace' => 'Semeru\Rtpo\Presentation\Controllers',
+        'controller' => 'controller',
+        'action' => 'approveRescheduleSIK'
+    ],
+    [
+        'pattern' => '/rtpo/request_mbp_to_site_down',
+        'method' => 'GET',
+        'namespace' => 'Semeru\Rtpo\Presentation\Controllers',
+        'controller' => 'controller',
+        'action' => 'cancelRequestMBP'
+    ],
+    [
+        'pattern' => '/rtpo/request_mbp_to_site_down',
+        'method' => 'GET',
+        'namespace' => 'Semeru\Rtpo\Presentation\Controllers',
+        'controller' => 'controller',
+        'action' => 'getDetailRescheduleSIK'
+    ],
+    [
+        'pattern' => '/rtpo/request_mbp_to_site_down',
+        'method' => 'GET',
+        'namespace' => 'Semeru\Rtpo\Presentation\Controllers',
+        'controller' => 'controller',
+        'action' => 'getListRescheduleSIKAction'
+    ],
+    [
+        'pattern' => '/rtpo/request_mbp_to_site_down',
+        'method' => 'GET',
+        'namespace' => 'Semeru\Rtpo\Presentation\Controllers',
+        'controller' => 'controller',
+        'action' => 'getTiketMBPTidakDikerjakanAction'
+    ],
+    [
+        'pattern' => '/rtpo/request_mbp_to_site_down',
+        'method' => 'GET',
+        'namespace' => 'Semeru\Rtpo\Presentation\Controllers',
+        'controller' => 'controller',
+        'action' => 'updateRescheduleSIKAction'
+    ],
+    [
+        'pattern' => '/rtpo/request_mbp_to_site_down',
+        'method' => 'GET',
+        'namespace' => 'Semeru\Rtpo\Presentation\Controllers',
+        'controller' => 'controller',
+        'action' => 'updateRescheduleSIKAction'
     ],
 ];
